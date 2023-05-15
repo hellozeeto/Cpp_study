eb5f9ece353c3391e24c2d0198eb07883d990161
hellozeeto <tktma1712@gmail.com>
2023-05-16 01:10:54 +0900

    패스트캠퍼스 공부

---
+++ C++/Project1/main.cpp
@@ -0,0 +1,14 @@
+
+int main()
+{
+	//자료형 (크기 단위 : byte)
+	//정수형 : char(1), short(2), int(4), long(4), long long(8)
+	//실수형 : float(4), double(8)
+
+	int i = 0;
+
+	//에드 커밋 푸쉬 연습
+	// 수정 정수 수민
+	int b = 3;
+	return 0;
+}
